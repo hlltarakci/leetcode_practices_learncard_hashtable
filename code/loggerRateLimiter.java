80bcc90e54a7bf8f3a50b1d7bd08beb3b77e33af
Hilal Tarakci <hlltarakci@gmail.com>
2020-05-09 22:28:31 +0300

    Create loggerRateLimiter.java

---
+++ code/loggerRateLimiter.java
@@ -0,0 +1,32 @@
+// https://leetcode.com/explore/learn/card/hash-table/184/comparison-with-other-data-structures/1122/
+
+class Logger {
+    /*
+        time: O(1)
+        space: O(num of messages)
+    */
+
+    Map<String, Integer> map;
+    
+    /** Initialize your data structure here. */
+    public Logger() {
+        map = new HashMap<>();
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        if(map.containsKey(message) && (timestamp - map.get(message)) < 10)  
+            return false;
+            
+            map.put(message, timestamp);
+            return true;
+    }
+}
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
